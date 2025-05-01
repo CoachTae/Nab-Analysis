4c4a2a4ea8f259efb556ce94f2388f14a9cf3a2e
CoachTae <skylar.clymer@gmail.com>
2025-05-01 16:21:53 -0400

    Test plot functions. Now plotting noise to check for normality

--- Test.py
+++ Test.py
@@ -20,7 +20,7 @@ elif User.lower() == 'arush':
 
 # Path for PyNab package
 sys.path.append(paths[0])
-# Path for deltarice (package created by David Matthew for Nab)
+# Path for deltarice (package created by David Matthews for Nab)
 sys.path.append(paths[1])
 import nabPy as Nab
 import h5py
@@ -30,6 +30,16 @@ Currently just trying to replicate the Basic.ipynb file to make sure
 everything works on this computer. Also to help learn the PyNab package,
 the methods available in it, and how to use it overall.
 '''
+
+
+# This "try" statement is for users using WSL which has no graphical display
+# On normal systems or with systems without tkinter, it will just pass instead.
+try:
+    import matplotlib
+    matplotlib.use('TkAgg')
+except ImportError:
+    pass # Fallback to system defaults
+
 
 # Load in data
 data = Nab.File(paths[2]+"Run5730_1.h5")
@@ -48,24 +58,52 @@ single = data.singleWaves()
         'cusp': [risetime, flat top length, decay rate, (threshold percement, mean, shift)]
         'doubletrap': [risetime, flat top length, decay rate, (threshold percent, mean, shift)]'''
 
+
+
+#-------------------------------------------------------------------------------
+
+counts = {}
+for i in range(10000):
+    try:
+        data = noise.wave(i)
+        for reading in data:
+            if reading in counts.keys():
+                counts[reading] += 1
+            else:
+                counts[reading] = 1
+    except:
+        break
+
+print(f'{i} waveforms recorded.')
+
+x = list(counts.keys())
+y = list(counts.values())
+
+plt.plot(x,y, 'o')
+plt.show()
+sys.exit()
+
+
+
 filter_settings = [1250, 50, 1250]
 
 # Coincidence waves energy timings
-Stimings = single.determineEnergyTiming(method='trap', params=filter_settings)
-print(Stimings)
+Ctimings = coinc.determineEnergyTiming(method='trap', params=filter_settings)
+print(Ctimings)
 
 # Plotting energies of coincidence waves
-fig, ax = plt.subplots(figsize(10, 5))
+fig, ax = plt.subplots(figsize=(10, 5))
 fig.patch.set_facecolor('xkcd:white')
-fig.text(0.5, -0.05, "Generated with: single.determineEnergyTiming(method='trap', params=filter_settings)", ha='center')
+fig.text(0.5, -0.05, "Generated with: coinc.determineEnergyTiming(method='trap', params=filter_settings)", ha='center')
 ax.set_xlabel('ADC Channel')
 ax.set_ylabel('Counts')
 ax.grid(True)
 ax.set_title('Energy Histogram \n Singles Data')
-Stimings.hist('energy', bins = Nab.np.arange(0,6000))
+Ctimings.hist('energy', bins = Nab.np.arange(0,6000))
 plt.xlim(0,6000)
 plt.ylim(0,20)
-Stimings.data().columns
+Ctimings.data().columns
+plt.show()
 
 
 
