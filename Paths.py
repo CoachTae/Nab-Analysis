9357eeeb6bb6204237c93bee83861375b342aacf
CoachTae <skylar.clymer@gmail.com>
2025-07-10 15:34:27 -0700

    Histogram of source data in Test.py

--- Paths.py
+++ Paths.py
@@ -2,5 +2,8 @@ Skylar_Paths = ["C:/Users/ricardo/Downloads/pyNab-master/pyNab-master/src", #PyN
                 "C:/Users/ricardo/Downloads/deltarice-master/deltarice-master/build/lib.linux-x86_64-3.10", #deltarice build folder
                 "/mnt/c/Users/ricardo/Downloads/"] #Folder where files are stored
                 
+Skylar_Home = ["/mnt/s/Downloads/pyNab/src/nabPy",
+               "S:/Downloads/deltarice-master/deltarice-master/build/lib.linux-x86_64-cpython-312",
+               "/mnt/s/Downloads/"]
 
 Arush_Paths = []
--- Test.py
+++ Test.py
@@ -9,12 +9,16 @@ import Paths
 import Plotting
 import json
 
+
 start_time = time.time()
-User = 'Skylar'
+User = 'Skylar2'
 
 
 if User.lower() == 'skylar':
     paths = Paths.Skylar_Paths
+
+elif User.lower() == "skylar2":
+    paths = Paths.Skylar_Home
 
 elif User.lower() == 'arush':
     paths = Paths.Arush_Paths
@@ -27,11 +31,6 @@ sys.path.append(paths[1])
 import nabPy as Nab
 import h5py
 
-'''
-Currently just trying to replicate the Basic.ipynb file to make sure
-everything works on this computer. Also to help learn the PyNab package,
-the methods available in it, and how to use it overall.
-'''
 
 
 # This "try" statement is for users using WSL which has no graphical display
@@ -42,101 +41,44 @@ try:
 except ImportError:
     pass # Fallback to system defaults
 
-run = Nab.DataRun(paths[2], 7616)
+
+
+run = Nab.DataRun(paths[2], 8118)
 parameters = run.parameterFile()
 
 
 #noise = run.noiseWaves()
-regcoinc = run.coincWaves()
-coinc = run.coincWaves().headers()
+#regcoinc = run.coincWaves()
+#coinc = run.coincWaves().headers()
+singles = run.singleWaves()
+singles_headers = run.singleWaves().headers()
 
+# Filter for populated pixel (1061) indices
+singles_indices = singles_headers[singles_headers['pixel'] == 1061].index.tolist()
 
-print(f'Initial # of Electrons: {len(regcoinc.waves())}')
+# Cut for only waveforms from most populated pixel
+singles.defineCut('custom', singles_indices)
 
-# Find median timestamp value
-# This has been edited to where it's no longer the median
-    # Instead it finds the quarter-marked timestamp.
-median_timestamp = coinc["timestamp"].quantile(0.25)
-
-# Filter out electrons
+# Apply trap filter and determine energies
 filter_settings = [1250, 50, 1250]
-regcoinc.defineCut("hit type", "=", 2)
+singles_energies = singles.determineEnergyTiming('trap', params=filter_settings)
 
-#Cut half of the electrons out by timestamp
-regcoinc.defineCut("timestamp", "<", median_timestamp)
+# Get headers object w/ energy column
+energies_headers = singles_energies.data()
 
-print(f'After timestamp cut: {len(regcoinc.waves())}')
-# Run Trap filter for energies
-coinc_energies = regcoinc.determineEnergyTiming(method='trap', params=filter_settings, batchsize=10)
+# Pull just the energies
+energies = energies_headers['energy']*0.3
 
-pd_df = coinc_energies.data()
-energies = pd_df["energy"]*0.3
-energies = energies.tolist()
-print(energies)
+# Histogram
+bin_size = 0.2
+min_energy = energies.min()
+max_energy = energies.max()
+bins = np.arange(min_energy, max_energy + bin_size, bin_size)
 
-data_dict = {
-    "Electron Energy": energies
-    }
-
-with open("NabData.json", 'w') as f:
-    json.dump(data_dict, f)
-print("Test completed!")
-sys.exit()
-
-
-
-
-#------------------------------------------------------------------------------
-#-----------------------------------------------------------------------------------
-#----------------------------------------------------------------------------------------
-
-
-
-
-# Load in data
-data = Nab.File(paths[2]+"Run7597_0.h5")
-
-
-
-# Extract coincidence waveforms
-coinc = data.coincWaves()
-pulsr = data.pulsrWaves()
-noise = data.noiseWaves()
-single = data.singleWaves()
-
-'''params: list with varying elements depending on the method passed
-        (optional parameters shown in parenthesis)
-        'trap': [risetime, flat top length, decay rate, (threshold percent, mean, shift)]
-        'cusp': [risetime, flat top length, decay rate, (threshold percement, mean, shift)]
-        'doubletrap': [risetime, flat top length, decay rate, (threshold percent, mean, shift)]'''
-
-
-
-
-filter_settings = [1250, 50, 1250]
-
-# Coincidence waves energy timings
-Ctimings = coinc.determineEnergyTiming(method='trap', params=filter_settings)
-print(Ctimings)
-
-# Plotting energies of coincidence waves
-fig, ax = plt.subplots(figsize=(10, 5))
-fig.patch.set_facecolor('xkcd:white')
-fig.text(0.5, -0.05, "Generated with: coinc.determineEnergyTiming(method='trap', params=filter_settings)", ha='center')
-ax.set_xlabel('ADC Channel')
-ax.set_ylabel('Counts')
-ax.grid(True)
-ax.set_title('Energy Histogram \n Singles Data')
-Ctimings.hist('energy', bins = Nab.np.arange(0,6000))
-plt.xlim(0,6000)
-plt.ylim(0,20)
-Ctimings.data().columns
+plt.hist(energies, bins=bins)
+plt.xlabel('Energy')
+plt.ylabel('Count')
 plt.show()
 
 
-
-
-
-#------------------------------------------------------------------------------
-# Print something to show that the code has resolved completely.
-print("Test successful!")
+print("TEST COMPLETED!")
